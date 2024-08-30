923a169621075b01aed04eeb2b583aad1597e758
Yacine Bakhti <yacinebakhti1mi@gmail.com>
2024-08-30 19:50:44 +0100

    chore: remove unused files

--- src/config/global-config.ts
+++ src/config/global-config.ts
@@ -7,12 +7,10 @@ export const drawerWidth = 200;
 
 import en from "@/assets/header/en.svg";
 import fr from "@/assets/header/fr.svg";
-import ar from "@/assets/header/ar.svg";
 
 export const defaultLanguage = "en";
 
 export const languages = [
   { code: "en", flagCode: "us", name: "English", flag: en },
   { code: "fr", flagCode: "fr", name: "French", flag: fr },
-  // { code: "ar", flagCode: "ar", name: "Arabic", flag: ar },
 ];
